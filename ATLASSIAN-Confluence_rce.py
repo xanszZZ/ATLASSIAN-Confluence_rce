4d7c41fbbea270ad13dc3384fc32d311b895cb88
xanszZZ <115653953+xanszZZ@users.noreply.github.com>
2022-10-16 23:09:09 +0800

    Update ATLASSIAN-Confluence_rce.py

--- ATLASSIAN-Confluence_rce.py
+++ ATLASSIAN-Confluence_rce.py
@@ -9,20 +9,20 @@ from pocsuite3.api import (
 
 
 class ACrcePOC(POCBase):
-    vulID = "1571"  # ssvid ID 如果是提交漏洞的同时提交 PoC,则写成 0
+    vulID = "1571"  # ssvid ID 
     version = "1"  # 默认为1
     author = "xans"  # PoC作者的大名
-    vulDate = "2022-06-03"  # 漏洞公开的时间,不知道就写今天
+    vulDate = "2022-06-03"  # 漏洞公开的时间
     createDate = "2022-10-16"  # 编写 PoC 的日期
-    updateDate = "2022-10-16"  # PoC 更新的时间,默认和编写时间一样
-    references = ["https://www.exploit-db.com/exploits/50952"]  # 漏洞地址来源,0day不用写
+    updateDate = "2022-10-16"  # PoC 更新的时间
+    references = ["https://www.exploit-db.com/exploits/50952"]  # 漏洞地址来源
     name = "ATLASSIAN-Confluence   rce 远程命令PoC"  # PoC 名称
     appPowerLink = ""  # 漏洞厂商主页地址
     appName = "Atlassian"  # 漏洞应用名称
     appVersion = "1.3.0版本至7.4.17之前版本、7.13.0版本至7.13.7之前版本、7.14.0版本至7.14.3之前版本、7.15.0版本至 7.15.2之前版本、7.16.0版本至7.16.4之前版本、7.17.0版本至7.17.4之前版本、7.18.0版本至7.18.1之前版本"  # 漏洞影响版本
     vulType = VUL_TYPE.CODE_EXECUTION  # 漏洞类型,类型参考见 漏洞类型规范表
     category = POC_CATEGORY.EXPLOITS.WEBAPP
-    samples = []  # 测试样列,就是用 PoC 测试成功的网站
+    samples = []  # 测试样列
     install_requires = []  # PoC 第三方模块依赖，请尽量不要使用第三方模块，必要时请参考《PoC第三方模块依赖说明》填写
     desc = """
                 Atlassian Confluence Server是澳大利亚Atlassian公司的一套具有企业知识管理功能，并支持用于构建企业WiKi的协同软件的服务器版本。
@@ -57,7 +57,7 @@ class ACrcePOC(POCBase):
 
     def _verify(self):
         result = {}
-        res = self._check()  # res就是返回的结果列表
+        res = self._check() 
         if res:
             result['VerifyInfo'] = {}
             result['VerifyInfo']['Info'] = self.name
